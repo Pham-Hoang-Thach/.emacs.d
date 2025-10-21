50d8586caf9017e3276a4641889ba37563c15b41
Pham Hoang Thach <phamhoangthach@gmail.com>
2025-10-21 10:43:37 +0700

    Separate MELPA and GITHUB packages

--- init.el
+++ init.el
@@ -15,6 +15,10 @@
 (require 'bind-key)                
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
+;; compile if there is newer nerd icons
+(dolist (dir (file-expand-wildcards "~/.emacs.d/elpa/nerd-icons-*/"))
+  (when (file-directory-p dir)
+    (byte-recompile-directory dir 0 t)))
 ;; start every frame maximized
 ;;(add-to-list 'default-frame-alist '(fullscreen . maximized))
 ;;(if (eq initial-window-system 'x)          ; if started by emacs command or desktop file
@@ -35,7 +39,19 @@
  ;; If there is more than one, they won't work right.
  '(doc-view-continuous t)
  '(org-log-into-drawer t)
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(all-the-icons-ivy auto-package-update clang-format command-log-mode
+                       company-c-headers conda counsel djvu
+                       doom-modeline doom-themes ellama ess
+                       flycheck-posframe helm-bibtex hydra jinx
+                       jupyter magit mcq-check-missing-correct-answers
+                       mcq-export-cleaned-questions
+                       mcq-extract-correct-answers
+                       mcq-persistent-org-combo-shuffler
+                       multiple-cursors nov olivetti org-download
+                       org-fragtog org-noter-pdftools org-ref
+                       org-roam-bibtex org-roam-ui org-superstar
+                       solaire-mode sqlite3 transpose-frame yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
